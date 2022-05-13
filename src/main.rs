eeeb5a58d406f112cc8b4e761a225ee1fe805df5
asdqdsa <asdqdsa@protonmail.com>
2022-05-13 22:37:42 +0400

    init

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-
-    println!("Please input your guess.");
-
+    println!("guess the number");
+    println!("input the number");
     let mut guess = String::new();
-
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    println!("Your number is: {}", guess);
 }
